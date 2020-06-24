f91b912cb71279d185ef19894c201ada847952f3
Alex Pawelko <hisaysajp@gmail.com>
2020-06-24 15:48:43 -0600

    basic boilerplate

---
+++ src/main/java/com/podcrash/squadassault/Main.java
@@ -0,0 +1,32 @@
+package com.podcrash.squadassault;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    private static Main instance;
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        registerCommands();
+        registerListeners();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    private void registerCommands() {
+
+    }
+
+    private void registerListeners() {
+
+    }
+
+}
