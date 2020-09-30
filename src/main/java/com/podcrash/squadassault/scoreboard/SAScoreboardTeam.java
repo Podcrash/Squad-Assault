babdfb89a4d4b56cc6dab1aff582ab8a8fdeefd5
kytobias <kytobias123@gmail.com>
2020-09-30 15:20:58 -0400

    Started conversion of bukkit scoreboards to packets

--- src/main/java/com/podcrash/squadassault/scoreboard/SAScoreboardTeam.java
+++ src/main/java/com/podcrash/squadassault/scoreboard/SAScoreboardTeam.java
@@ -1,5 +1,6 @@
 package com.podcrash.squadassault.scoreboard;
 
+import com.packetwrapper.abstractpackets.WrapperPlayServerScoreboardTeam;
 import com.podcrash.squadassault.Main;
 import com.podcrash.squadassault.game.PlayerStats;
 import com.podcrash.squadassault.game.SAGame;
@@ -11,6 +12,7 @@ import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SAScoreboardTeam {
@@ -24,26 +26,44 @@ public class SAScoreboardTeam {
         registerSides(game);
     }
 
+    /*
+    Create scoreboard team using packets
+    Send packet to players of same team to have name visibility always
+    Send packet to players of other team to have name visibility to never
+    Send packet to spectators to have name visibility always
+     */
     public void registerSides(SAGame game) {
         for(Player player : Main.getGameManager().getTeam(game, SATeam.Team.OMEGA).getPlayers()) {
             PlayerStats stats = game.getStats().get(player.getUniqueId());
-            Team registerNewTeam = scoreboard.registerNewTeam(player.getName());
-            registerNewTeam.setPrefix("O");
-            registerNewTeam.setSuffix(stats.getKills() + " - " + stats.getDeaths());
-            registerNewTeam.setNameTagVisibility(NameTagVisibility.HIDE_FOR_OTHER_TEAMS);
-//            NmsUtils.hideNametag(registerNewTeam);
-            registerNewTeam.addEntry(player.getName());
-            teams.add(registerNewTeam);
+//            Team registerNewTeam = scoreboard.registerNewTeam(player.getName());
+//            registerNewTeam.setPrefix("O");
+//            registerNewTeam.setSuffix(stats.getKills() + " - " + stats.getDeaths());
+//            registerNewTeam.setNameTagVisibility(NameTagVisibility.HIDE_FOR_OTHER_TEAMS);
+////            NmsUtils.hideNametag(registerNewTeam);
+//            registerNewTeam.addEntry(player.getName());
+//            teams.add(registerNewTeam);
+            WrapperPlayServerScoreboardTeam scoreboardTeam = new WrapperPlayServerScoreboardTeam();
+            scoreboardTeam.setName(player.getName());
+            scoreboardTeam.setPrefix("O");
+            scoreboardTeam.setSuffix(stats.getKills() + " - " + stats.getDeaths());
+            scoreboardTeam.setPlayers(Collections.singletonList(player.getName()));
+            setTagVisibility(scoreboardTeam, Main.getGameManager().getTeam(game, SATeam.Team.OMEGA).getPlayers(), Main.getGameManager().getTeam(game, SATeam.Team.ALPHA).getPlayers());
         }
         for(Player player : Main.getGameManager().getTeam(game, SATeam.Team.ALPHA).getPlayers()) {
             PlayerStats stats = game.getStats().get(player.getUniqueId());
-            Team registerNewTeam = scoreboard.registerNewTeam(player.getName());
-            registerNewTeam.setPrefix("A");
-            registerNewTeam.setSuffix(stats.getKills() + " - " + stats.getDeaths());
-            registerNewTeam.setNameTagVisibility(NameTagVisibility.HIDE_FOR_OTHER_TEAMS);
-//            NmsUtils.hideNametag(registerNewTeam);
-            registerNewTeam.addEntry(player.getName());
-            teams.add(registerNewTeam);
+//            Team registerNewTeam = scoreboard.registerNewTeam(player.getName());
+//            registerNewTeam.setPrefix("A");
+//            registerNewTeam.setSuffix(stats.getKills() + " - " + stats.getDeaths());
+//            registerNewTeam.setNameTagVisibility(NameTagVisibility.HIDE_FOR_OTHER_TEAMS);
+////            NmsUtils.hideNametag(registerNewTeam);
+//            registerNewTeam.addEntry(player.getName());
+//            teams.add(registerNewTeam);
+            WrapperPlayServerScoreboardTeam scoreboardTeam = new WrapperPlayServerScoreboardTeam();
+            scoreboardTeam.setName(player.getName());
+            scoreboardTeam.setPrefix("O");
+            scoreboardTeam.setSuffix(stats.getKills() + " - " + stats.getDeaths());
+            scoreboardTeam.setPlayers(Collections.singletonList(player.getName()));
+            setTagVisibility(scoreboardTeam, Main.getGameManager().getTeam(game, SATeam.Team.ALPHA).getPlayers(), Main.getGameManager().getTeam(game, SATeam.Team.OMEGA).getPlayers());
         }
     }
 
@@ -75,5 +95,16 @@ public class SAScoreboardTeam {
         team.setPrefix(Main.getGameManager().getTeam(game, player) == SATeam.Team.OMEGA ? "O" : "A");
         team.setSuffix(stats.getKills() + " - " + stats.getDeaths());
         NmsUtils.hideNametag(team);
+    }
+
+    private void setTagVisibility(WrapperPlayServerScoreboardTeam scoreboardTeam, List<Player> sameTeam, List<Player> otherTeam) {
+        scoreboardTeam.setNameTagVisibility("always");
+        for (Player player : sameTeam) {
+            scoreboardTeam.sendPacket(player);
+        }
+        scoreboardTeam.setNameTagVisibility("never");
+        for (Player player : otherTeam) {
+            scoreboardTeam.sendPacket(player);
+        }
     }
 }
